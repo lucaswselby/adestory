68cd94d24cbe598bf4801749852719565239c796
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2025-04-11 00:42:40 -0700

    row 2 of houses tree

--- housesList.js
+++ housesList.js
@@ -1,9 +1,9 @@
 class House {
-    constructor(name, original, active, foundedBy, people) {
+    constructor(name, original, active, founded, people) {
         this._name = name;
         this._original = original;
         this._active = active;
-        this._foundedBy = foundedBy;
+        this._founded = founded;
         this._people = people;
     }
     get name() {
@@ -15,86 +15,86 @@ class House {
     get active() {
         return this._active;
     }
-    get foundedBy() {
-        return this._foundedBy;
+    get founded() {
+        return this._founded;
     }
-    set foundedBy(foundedBy) {
-        this._foundedBy = foundedBy;
+    set founded(founded) {
+        this._founded = founded;
     }
     get people() {
         return this._people;
     }
 }
-const ahmedhan = new House("AHMEDHAN", true, true, null, ["", "", "", "", "", ""]);
-const idirissi = new House("IDIRISSI", true, true, ahmedhan, ["", "", "", "", "", "", ""]);
-const tshireletso = new House("TSHIRELETSO", true, false, idirissi, ["", "", "", "", "", "", ""]);
-const palladino = new House("PALLADINO", true, false, tshireletso, ["", "", "", "", ""]);
-const pendergast = new House("PENDERGAST", true, true, palladino, ["", "", "", "", "", ""]);
-const almufti = new House("AL-MUFTI", true, false, pendergast, ["", "", "", "", "", "", "", ""]);
-const etienne = new House("ETIENNE", true, true, almufti, ["", "", "", "", "", "", ""]);
-const otieno = new House("OTIENO", true, true, etienne, ["", "", "", "", "", "", "", ""]);
-const stJoseph = new House("ST. JOSEPH", true, true, otieno, ["", "", "", "", "", "", "", "", "", "", "", ""]);
-const patenaude = new House("PATENAUDE", true, false, stJoseph, ["", "", "", "", "", "", "", ""]);
-const blythe = new House("BLYTHE", true, true, patenaude, ["", "", "", "", "", "", "", ""]);
-const arkwright = new House("ARKWRIGHT", true, false, blythe, ["", "", "", "", "", "", "", ""]);
-const botzami = new House("BOTZAMI", true, false, arkwright, ["", "", "", "", "", "", ""]);
-const cromwell = new House("CROMWELL", true, true, botzami, ["", "", "", "", "", "", ""]);
-const zyma = new House("ZYMA", true, false, cromwell, ["", "", "", "", ""]);
-const tran = new House("TRAN", true, true, zyma, ["", "", "", "", "", ""]);
-const carvalho = new House("CARVALHO", true, true, tran, ["", "", "", "", "", ""]);
-const figueroa = new House("FIGUEROA", true, true, carvalho, ["", "", "", "", "", ""]);
-const vang = new House("VANG", true, true, figueroa, ["", "", "", "", "", ""]);
-const liavaa = new House("LIAVA'A", true, false, vang, ["", "", "", "", "", "", ""]);
-const merovingian = new House("MEROVINGIAN", true, false, liavaa, ["", "", "", "", "", "", ""]);
-const delaurante = new House("DeLAURANTE", true, true, merovingian, ["", "", "", "", "", "", "", "", ""]);
-ahmedhan.foundedBy = delaurante;
-const vanDerReede = new House("VAN DER REEDE", false, false, arkwright, ["", "", "", "", "", "", ""]);
-const adakai = new House("ADAKAI", false, false, vanDerReede, ["", "", "", "", "", "", ""]);
-const alavonzaro = new House("ALAVONZARO", false, true, pendergast, ["", "", "", "", "", "", ""]);
-const bennett = new House("BENNETT", false, true, merovingian, ["", "", "", "", "", "", "", "", "", "", "", ""]);
-const paora = new House("PAORA", false, false, pendergast, ["", "", "", "", "", ""]);
-const godfrey = new House("GODFREY", false, false, paora, ["", "", "", "", "", "", "", ""]);
-const beraszafer = new House("BERASZAFER", false, false, godfrey, ["", "", "", "", "", "", ""]);
-const cappallo = new House("CAPPALLO", false, false, adakai, ["", "", "", "", "", "", "", ""]);
-const quijada = new House("QUIJADA", false, false, figueroa, ["", "", "", "", "", "", "", ""]);
-const cronje = new House("CRONJE", false, false, quijada, ["", "", "", "", "", "", ""]);
-const castillo = new House("CASTILLO", false, true, cronje, ["", "", "", "", "", ""]);
-const crassus = new House("CRASSUS", false, true, castillo, ["", "", "", "", "", ""]);
-const faragulski = new House("FARAGULSKI", false, false, palladino, ["", "", "", "", "", ""]);
-const mavoungou = new House("MAVOUNGOU", false, true, faragulski, ["", "", "", "", "", ""]);
-const espinosa = new House("ESPINOSA", false, false, mavoungou, ["", "", "", "", "", "", "", ""]);
-const dovorak = new House("DOVORAK", false, true, espinosa, ["", "", "", "", "", "", "", "", "", "", ""]);
-const eabhacdon = new House("EABHACDON", false, false, null, ["", "", "", "", "", "", ""]);
-const gallig = new House("GALLIG", false, true, almufti, ["", "", "", "", "", "", ""]);
-const haryanka = new House("HARYANKA", false, true, gallig, ["", "", "", "", "", "", "", ""]);
-const salucnar = new House("SALUCNAR", false, false, mavoungou, ["", "", "", "", ""]);
-const ohara = new House("O'HARA", false, true, salucnar, ["", "", "", "", "", "", ""]);
-const havili = new House("HAVILI", false, false, ohara, ["", "", "", "", "", "", "", ""]);
-const jolicoeur = new House("JOLICOEUR", false, false, delaurante, ["", "", "", "", "", ""]);
-const haylock = new House("HAYLOCK", false, true, jolicoeur, ["", "", "", "", "", "", ""]);
-const huang = new House("HUANG", false, true, vanDerReede, ["", "", "", "", "", "", ""]);
-const issako = new House("ISSAKO", false, false, palladino, ["", "", "", "", "", "", "", "", ""]);
-const kasabian = new House("KASABIAN", false, false, quijada, ["", "", "", "", "", "", "", ""]);
-const krowe = new House("KROWE", false, true, godfrey, ["", "", "", "", "", "", "", "", "", ""]);
-const leskai = new House("LESKAI", false, true, jolicoeur, ["", "", "", "", "", "", ""]);
-const li = new House("LI", false, true, huang, ["", "", "", "", "", ""]);
-const maldonado = new House("MALDONADO", false, false, cronje, ["", "", "", "", "", "", ""]);
-const mavros = new House("MAVROS", false, true, cronje, ["", "", "", "", "", "", ""]);
-const nagata = new House("NAGATA", false, false, krowe, ["", "", "", "", "", "", ""]);
-const nakatsuomi = new House("NAKA-TSU-OMI", false, true, stJoseph, ["", "", "", "", "", ""]);
-const ragensterne = new House("RAGENSTERNE", false, true, tshireletso, ["", "", "", "", "", "", ""]);
-const ong = new House("ONG", false, true, ragensterne, ["", "", "", "", "", "", ""]);
-const rybamerc = new House("RYBAMERC", false, false, crassus, ["", "", "", "", "", "", ""]);
-const orlov = new House("ORLOV", false, true, rybamerc, ["", "", "", "", "", ""]);
-const vidrogotchen = new House("VIDROGOTCHEN", false, false, merovingian, ["", "", "", "", "", "", ""]);
-const phiri = new House("PHIRI", false, true, vidrogotchen, ["", "", "", "", "", ""]);
-const rashtrakurtha = new House("RASHTRAKURTHA", false, true, havili, ["", "", "", "", "", ""]);
-const salazar = new House("SALAZAR", false, true, phiri, ["", "", "", "", "", "", ""]);
-const sharrow = new House("SHARROW", false, false, cappallo, ["", "", "", "", "", "", ""]);
-const tatlonghari = new House("TATLONGHARI", false, true, cromwell, ["", "", "", "", "", ""]);
-const taylor = new House("TAYLOR", false, true, gallig, ["", "", "", "", "", ""]);
-const zaitsev = new House("ZAITSEV", false, true, orlov, ["", "", "", "", "", "", ""]);
-const waldington = new House("WALDINGTON", false, false, zaitsev, ["", "", "", "", "", "", ""]);
-const weirski = new House("WEIRSKI", false, true, rybamerc, ["", "", "", "", "", "", ""]);
-const xydias = new House("XYDIAS", false, true, godfrey, ["", "", "", "", "", "", ""]);
+const eabhacdon = new House("EABHACDON", true, false, [], ["", "", "", "", "", "", ""]);
+const tatlonghari = new House("TATLONGHARI", false, true, [], ["", "", "", "", "", ""]);
+const zyma = new House("ZYMA", true, false, null, ["", "", "", "", ""]);
+const cromwell = new House("CROMWELL", true, true, [zyma, tatlonghari], ["", "", "", "", "", "", ""]);
+const botzami = new House("BOTZAMI", true, false, [cromwell], ["", "", "", "", "", "", ""]);
+const sharrow = new House("SHARROW", false, false, [], ["", "", "", "", "", "", ""]);
+const cappallo = new House("CAPPALLO", false, false, [sharrow], ["", "", "", "", "", "", "", ""]);
+const adakai = new House("ADAKAI", false, false, [cappallo], ["", "", "", "", "", "", ""]);
+const li = new House("LI", false, true, [], ["", "", "", "", "", ""]);
+const huang = new House("HUANG", false, true, [li], ["", "", "", "", "", "", ""]);
+const vanDerReede = new House("VAN DER REEDE", false, false, [adakai, huang], ["", "", "", "", "", "", ""]);
+const arkwright = new House("ARKWRIGHT", true, false, [botzami, vanDerReede], ["", "", "", "", "", "", "", ""]);
+const blythe = new House("BLYTHE", true, true, [arkwright], ["", "", "", "", "", "", "", ""]);
+const patenaude = new House("PATENAUDE", true, false, [blythe], ["", "", "", "", "", "", "", ""]);
+const nakatsuomi = new House("NAKA-TSU-OMI", false, true, [], ["", "", "", "", "", ""]);
+const stJoseph = new House("ST. JOSEPH", true, true, [patenaude, nakatsuomi], ["", "", "", "", "", "", "", "", "", "", "", ""]);
+const otieno = new House("OTIENO", true, true, [stJoseph], ["", "", "", "", "", "", "", ""]);
+const etienne = new House("ETIENNE", true, true, [otieno], ["", "", "", "", "", "", ""]);
+const haryanka = new House("HARYANKA", false, true, [], ["", "", "", "", "", "", "", ""]);
+const taylor = new House("TAYLOR", false, true, [], ["", "", "", "", "", ""]);
+const gallig = new House("GALLIG", false, true, [haryanka, taylor], ["", "", "", "", "", "", ""]);
+const almufti = new House("AL-MUFTI", true, false, [etienne, gallig], ["", "", "", "", "", "", "", ""]);
+const alavonzaro = new House("ALAVONZARO", false, true, [], ["", "", "", "", "", "", ""]);
+const beraszafer = new House("BERASZAFER", false, false, [], ["", "", "", "", "", "", ""]);
+const nagata = new House("NAGATA", false, false, [], ["", "", "", "", "", "", ""]);
+const krowe = new House("KROWE", false, true, [nagata], ["", "", "", "", "", "", "", "", "", ""]);
+const xydias = new House("XYDIAS", false, true, [], ["", "", "", "", "", "", ""]);
+const godfrey = new House("GODFREY", false, false, [beraszafer, krowe, xydias], ["", "", "", "", "", "", "", ""]);
+const paora = new House("PAORA", false, false, [godfrey], ["", "", "", "", "", ""]);
+const pendergast = new House("PENDERGAST", true, true, [almufti, alavonzaro, paora], ["", "", "", "", "", ""]);
+const dovorak = new House("DOVORAK", false, true, [], ["", "", "", "", "", "", "", "", "", "", ""]);
+const espinosa = new House("ESPINOSA", false, false, [dovorak], ["", "", "", "", "", "", "", ""]);
+const rashtrakurtha = new House("RASHTRAKURTHA", false, true, [], ["", "", "", "", "", ""]);
+const havili = new House("HAVILI", false, false, [rashtrakurtha], ["", "", "", "", "", "", "", ""]);
+const ohara = new House("O'HARA", false, true, [havili], ["", "", "", "", "", "", ""]);
+const salucnar = new House("SALUCNAR", false, false, [ohara], ["", "", "", "", ""]);
+const mavoungou = new House("MAVOUNGOU", false, true, [espinosa, salucnar], ["", "", "", "", "", ""]);
+const faragulski = new House("FARAGULSKI", false, false, [mavoungou], ["", "", "", "", "", ""]);
+const issako = new House("ISSAKO", false, false, [], ["", "", "", "", "", "", "", "", ""]);
+const palladino = new House("PALLADINO", true, false, [pendergast, faragulski, issako], ["", "", "", "", ""]);
+const ong = new House("ONG", false, true, [], ["", "", "", "", "", "", ""]);
+const ragensterne = new House("RAGENSTERNE", false, true, [ong], ["", "", "", "", "", "", ""]);
+const tshireletso = new House("TSHIRELETSO", true, false, [palladino, ragensterne], ["", "", "", "", "", "", ""]);
+const idirissi = new House("IDIRISSI", true, true, [tshireletso], ["", "", "", "", "", "", ""]);
+const ahmedhan = new House("AHMEDHAN", true, true, [idirissi], ["", "", "", "", "", ""]);
+const haylock = new House("HAYLOCK", false, true, [], ["", "", "", "", "", "", ""]);
+const leskai = new House("LESKAI", false, true, [], ["", "", "", "", "", "", ""]);
+const jolicoeur = new House("JOLICOEUR", false, false, [haylock, leskai], ["", "", "", "", "", ""]);
+const delaurante = new House("DeLAURANTE", true, true, [ahmedhan, jolicoeur], ["", "", "", "", "", "", "", "", ""]);
+const bennett = new House("BENNETT", false, true, [], ["", "", "", "", "", "", "", "", "", "", "", ""]);
+const salazar = new House("SALAZAR", false, true, [], ["", "", "", "", "", "", ""]);
+const phiri = new House("PHIRI", false, true, [salazar], ["", "", "", "", "", ""]);
+const vidrogotchen = new House("VIDROGOTCHEN", false, false, [phiri], ["", "", "", "", "", "", ""]);
+const merovingian = new House("MEROVINGIAN", true, false, [delaurante, bennett, vidrogotchen], ["", "", "", "", "", "", ""]);
+const liavaa = new House("LIAVA'A", true, false, [merovingian], ["", "", "", "", "", "", ""]);
+const vang = new House("VANG", true, true, [liavaa], ["", "", "", "", "", ""]);
+const waldington = new House("WALDINGTON", false, false, [], ["", "", "", "", "", "", ""]);
+const zaitsev = new House("ZAITSEV", false, true, [waldington], ["", "", "", "", "", "", ""]);
+const orlov = new House("ORLOV", false, true, [zaitsev], ["", "", "", "", "", ""]);
+const weirski = new House("WEIRSKI", false, true, [], ["", "", "", "", "", "", ""]);
+const rybamerc = new House("RYBAMERC", false, false, [orlov, weirski], ["", "", "", "", "", "", ""]);
+const crassus = new House("CRASSUS", false, true, [rybamerc], ["", "", "", "", "", ""]);
+const castillo = new House("CASTILLO", false, true, [crassus], ["", "", "", "", "", ""]);
+const maldonado = new House("MALDONADO", false, false, [], ["", "", "", "", "", "", ""]);
+const mavros = new House("MAVROS", false, true, [], ["", "", "", "", "", "", ""]);
+const cronje = new House("CRONJE", false, false, [castillo, maldonado, mavros], ["", "", "", "", "", "", ""]);
+const kasabian = new House("KASABIAN", false, false, [], ["", "", "", "", "", "", "", ""]);
+const quijada = new House("QUIJADA", false, false, [cronje, kasabian], ["", "", "", "", "", "", "", ""]);
+const figueroa = new House("FIGUEROA", true, true, [vang, quijada], ["", "", "", "", "", ""]);
+const carvalho = new House("CARVALHO", true, true, [figueroa], ["", "", "", "", "", ""]);
+const tran = new House("TRAN", true, true, [carvalho], ["", "", "", "", "", ""]);
+zyma.founded = [tran];
 const houses = [adakai, ahmedhan, alavonzaro, almufti, arkwright, bennett, beraszafer, blythe, botzami, cappallo, carvalho, castillo, crassus, cromwell, cronje, delaurante, dovorak, eabhacdon, espinosa, etienne, faragulski, figueroa, gallig, godfrey, haryanka, havili, haylock, huang, idirissi, issako, jolicoeur, kasabian, krowe, leskai, li, liavaa, maldonado, mavoungou, mavros, merovingian, nagata, nakatsuomi, ohara, ong, orlov, otieno, palladino, paora, patenaude, pendergast, phiri, quijada, ragensterne, rashtrakurtha, rybamerc, salazar, salucnar, sharrow, stJoseph, tatlonghari, taylor, tran, tshireletso, vanDerReede, vang, vidrogotchen, waldington, weirski, xydias, zaitsev, zyma];
--- housesTree.js
+++ housesTree.js
@@ -1,10 +1,26 @@
 // fill table header with original houses in order
-let unorderedOriginalHouses = houses.filter(house => {return house.original;});
+let unorderedOriginalHouses = houses.filter(house => {return house.original && house !== eabhacdon;});
 let originalHouses = unorderedOriginalHouses.map(house => {return null;});
 originalHouses[0] = unorderedOriginalHouses[0];
 for (let i = 1; i < originalHouses.length; i++) {
-    originalHouses[i] = originalHouses[i - 1].foundedBy;
+    originalHouses[i] = originalHouses[i - 1].founded[0];
 }
 originalHouses.forEach(house => {
-    document.getElementsByTagName("THEAD")[0].getElementsByTagName("TR")[0].innerHTML += `<td id="${house.name}">${house.name}<br>&#129120;founded</td>`;
+    document.getElementsByTagName("THEAD")[0].getElementsByTagName("TR")[0].innerHTML += `<td id="${house.name.replaceAll(".", "")}">${house.name}</td>`;
+});
+
+// fill table body with descendants of original houses
+let parentHouses = [...originalHouses];
+document.getElementsByTagName("TBODY")[0].innerHTML += "<tr></tr>";
+parentHouses.forEach(parentHouse => {
+    let childHouses = parentHouse.founded.filter(house => {return !parentHouses.includes(house)});
+    if (childHouses.length) {
+        document.getElementById(parentHouse.name.replaceAll(".", "")).colSpan = parseInt(document.getElementById(parentHouse.name.replaceAll(".", "")).colSpan) + childHouses.length - 1; // updates colspan of parent house
+        childHouses.forEach(childHouse => {
+            document.getElementsByTagName("TR")[document.getElementsByTagName("TR").length - 1].innerHTML += `<td id="${childHouse.name}">${childHouse.name}</td>`;
+        });
+    }
+    else {
+        document.getElementsByTagName("TR")[document.getElementsByTagName("TR").length - 1].innerHTML += "<td></td>"
+    }
 });
